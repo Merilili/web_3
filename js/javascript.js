1f1c7c086d99c922e4af691fbdd457ae1baa9e6d
Merilili <104058993+Merilili@users.noreply.github.com>
2022-04-20 11:43:43 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,10 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
-const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  maxZoom: 19,
- attribution: 'OpenStreetMap contributors',
+ attribution:
+  '&copy; <a href="https://www.openstreetmap.org/copywright">OpenStreetMap</a> contrubutors',
 })
+
 osm.addTo(map)
 
 async function addDistrictsGeoJson(url) {
@@ -12,5 +13,6 @@ async function addDistrictsGeoJson(url) {
  const polygons = L.geoJson(data)
  polygons.addTo(map)
 }
+
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 

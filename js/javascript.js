091747f91124a1c70d7184a7b23ac181d50eae05
Merilili <104058993+Merilili@users.noreply.github.com>
2022-04-20 11:45:13 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -14,5 +14,5 @@ async function addDistrictsGeoJson(url) {
  polygons.addTo(map)
 }
 
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+addDistrictsGeoJson('geojson_2/tartu_city_districts_edu.geojson')
 

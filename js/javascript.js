9dd82f20123d3addf1f50e8fc808e832ad9c0cbc
Merilili <104058993+Merilili@users.noreply.github.com>
2022-04-20 11:34:34 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
+
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data)
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+

7586adcfd001f118a40159c1c1ac19a007b554fa
Merilili <104058993+Merilili@users.noreply.github.com>
2022-04-27 11:28:37 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,1 +1,16 @@
+addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+// add geoJSON layer
+async function addGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const heatData = data.features.map(heatDataConvert)
+ console.log(heatData)
+}
+function heatDataConvert(feature) {
+ return [
+ feature.geometry.coordinates[1],
+ feature.geometry.coordinates[0],
+ feature.properties.area,
+ ]
+}
 

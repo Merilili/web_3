54481fdf6afd61f4cab86c924675afd604901d7a
Merilili <104058993+Merilili@users.noreply.github.com>
2022-05-02 21:18:10 +0300

    Update chloroplethmap.js

--- js/chloroplethmap.js
+++ js/chloroplethmap.js
@@ -1,13 +1,28 @@
-addGeoJson('geojson/tartu_city_districts_edu.geojson')
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+
+const osm =
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: 'OpenStreetMap contributors',
+})
+
+osm.addTo(map)
+6
+
+function defaultMapSettings() {
+map.setView([58.373523, 26.716045], 12)
+}
+
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 // add geoJSON layer
-async function addGeoJson(url) {
+async function addDistrictsGeoJson(url) {
  const response = await fetch(url)
  const data = await response.json()
  L.choropleth(data, {
- valueProperty: 'OBJECTID',
- scale: ['#ffffff', '#ff9900'],
+ valueProperty: 'TOWERS',
+ scale: ['#feebe2', '#7a0177'],
  steps: 5,
- mode: 'q', // q for quantile, e for equidistant
+ mode: 'e', // q for quantile, e for equidistant
  style: {
  color: '#fff',
  weight: 2,
